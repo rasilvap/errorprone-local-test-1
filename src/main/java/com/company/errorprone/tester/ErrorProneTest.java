8436767144c137c4725c542a0c03dbbcbdf08e8d
Agustin Martinez <agustin.martinez@sonaype.com>
2023-01-11 12:10:12 -0800

    trigger math abs negative

--- src/main/java/com/company/errorprone/tester/ErrorProneTest.java
+++ src/main/java/com/company/errorprone/tester/ErrorProneTest.java
@@ -3,9 +3,15 @@ package com.company.errorprone.tester;
 public class ErrorProneTest {
     public static void main(String[] args) {
         var stringArray = triggerAvoidObjectArrays();
+
     }
 
     public static String[] triggerAvoidObjectArrays() {
         return new String[]{"this","is","a","object","array"};
+    }
+
+    public static void triggerMathAbsoluteNegative() {
+        int veryNegative = Math.abs(Integer.MIN_VALUE);
+        long veryNegativeLong = Math.abs(Long.MIN_VALUE);
     }
 }
