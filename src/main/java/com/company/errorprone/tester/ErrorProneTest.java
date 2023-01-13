60e9857aeb49b7f4cbbc3f149aa47e81ad02d1c2
Agustin Martinez <agustin.martinez@sonaype.com>
2023-01-13 08:12:37 -0800

    testing errorprone 2.10.0

--- src/main/java/com/company/errorprone/tester/ErrorProneTest.java
+++ src/main/java/com/company/errorprone/tester/ErrorProneTest.java
@@ -6,11 +6,14 @@ import java.util.Random;
 public class ErrorProneTest {
     static final long NANOS_PER_DAY = 24 * 60 * 60 * 1000 * 1000 * 1000;
     public static void main(String[] args) {
-        String[] stringArray = triggerAvoidObjectArrays();
+        triggerArrayEquals();
+        triggerAvoidObjectArrays();
         triggerMathAbsoluteNegative();
+    }
+
+    public static void triggerArrayEquals() {
         String[] thisArr = new String[]{"this"};
         thisArr.equals(new String[]{"that"});
-        System.out.println(Arrays.toString(stringArray));
     }
 
     public static String[] triggerAvoidObjectArrays() {
@@ -18,10 +21,6 @@ public class ErrorProneTest {
     }
 
     public static void triggerMathAbsoluteNegative() {
-        int veryNegative = Math.abs(Integer.MIN_VALUE);
-        long veryNegativeLong = Math.abs(Long.MIN_VALUE);
-        System.out.println(veryNegative);
-        System.out.println(veryNegativeLong);
         Random random = new Random();
         Math.abs(random.nextInt());
     }
